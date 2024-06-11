57a9832aa9856e8a68da650d6ffe989b99ec113e
Nhat Anh <nmnanh1235@gmail.com>
2024-06-11 18:36:23 +0700

    Update ContentView.swift

--- Studious/ContentView.swift
+++ Studious/ContentView.swift
@@ -6,72 +6,51 @@
 //
 
 import SwiftUI
-import AVFoundation
 
 struct ContentView: View {
     @ObservedObject var viewModel = ViewModel()
     
-    let subjects = ["Math", "Literature", "English"] // Subjects array
-    let workTime = 25 * 60
-    let breakTime = 0 * 60
-
-    @State private var timer: Timer?
-    @State private var isTimerRunning = false
-    @State private var isBreakTime = false
-    @State private var timeRemaining = 25 * 60
-    @State private var selectedSubject = "Math" // Default subject
-
     var body: some View {
         ZStack {
-            Color(isBreakTime ? .white : .purple).edgesIgnoringSafeArea(.all)
+            Color(viewModel.isBreakTime ? .white : .purple).edgesIgnoringSafeArea(.all)
             
             VStack {
                 Spacer()
                 
-                // ComboBox for selecting subjects
-                Picker(selection: $selectedSubject, label: Text(selectedSubject)) {
-                    ForEach(subjects, id: \.self) { subject in
+                Picker(selection: $viewModel.selectedSubject, label: Text(viewModel.selectedSubject)) {
+                    ForEach(viewModel.subjects, id: \.self) { subject in
                         Text(subject)
                     }
                 }
-                .pickerStyle(MenuPickerStyle()) // Style the picker
-                .foregroundColor(isBreakTime ? .blue : .white)
+                .pickerStyle(MenuPickerStyle())
+                .foregroundColor(viewModel.isBreakTime ? .blue : .white)
                 .padding()
+                .onChange(of: viewModel.selectedSubject) { newValue in
+                    viewModel.updateTimes(for: newValue)
+                }
                 
                 ZStack {
                     Circle()
                         .stroke(lineWidth: 20)
                         .opacity(0.3)
                         .foregroundColor(.gray)
-                        .shadow(color: .black, radius: 10, x: 0, y: 0) // Add shadow effect
+                        .shadow(color: .black, radius: 10, x: 0, y: 0)
                     Circle()
-                        .trim(from: 0, to: CGFloat(timeRemaining) / CGFloat(isBreakTime ? self.breakTime : self.workTime)) // Max time for work: 25 minutes, for break: 5 minutes
+                        .trim(from: 0, to: CGFloat(viewModel.timeRemaining) / CGFloat(viewModel.isBreakTime ? viewModel.timings[viewModel.selectedSubject]!.break : viewModel.timings[viewModel.selectedSubject]!.work))
                         .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .round, lineJoin: .round))
-                        .foregroundColor(isBreakTime ? .blue : .green)
+                        .foregroundColor(viewModel.isBreakTime ? .blue : .green)
                         .rotationEffect(.degrees(-90))
                         .animation(.easeInOut)
                     
-                    Text("\(timeString(time: timeRemaining))")
+                    Text("\(viewModel.timeString(time: viewModel.timeRemaining))")
                         .font(.largeTitle)
-                        .foregroundColor(isBreakTime ? .black : .white)
+                        .foregroundColor(viewModel.isBreakTime ? .black : .white)
                 }
                 .frame(width: 200, height: 200)
                 
                 HStack(spacing: 20) {
                     Button(action: {
-                        if isTimerRunning {
-                            pauseTimer()
-                        } else {
-                            startTimer()
-                        }
-                    }) {
-                        Image(systemName: isTimerRunning ? "pause.circle.fill" : "play.circle.fill")
-                            .font(.largeTitle)
-                            .foregroundColor(isTimerRunning ? .orange : Color(red: 131/255, green: 180/255, blue: 255/255))
-                    }
-                    
-                    Button(action: {
-                        stopTimer()
+                        viewModel.stopTimer()
                     }) {
                         Image(systemName: "stop.circle.fill")
                             .font(.largeTitle)
@@ -79,7 +58,19 @@ struct ContentView: View {
                     }
                     
                     Button(action: {
-                        nextSession()
+                        if viewModel.isTimerRunning {
+                            viewModel.pauseTimer()
+                        } else {
+                            viewModel.startTimer()
+                        }
+                    }) {
+                        Image(systemName: viewModel.isTimerRunning ? "pause.circle.fill" : "play.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundColor(viewModel.isTimerRunning ? .orange : Color(red: 131/255, green: 180/255, blue: 255/255))
+                    }
+                    
+                    Button(action: {
+                        viewModel.nextSession()
                     }) {
                         Image(systemName: "arrow.right.circle.fill")
                             .font(.largeTitle)
@@ -91,60 +82,6 @@ struct ContentView: View {
                 Spacer()
             }
         }
-    }
-    
-    func startTimer() {
-        isTimerRunning = true
-        if timer == nil {
-            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-                if timeRemaining > 0 {
-                    timeRemaining -= 1
-                } else {
-                    timer?.invalidate()
-                    timer = nil
-                    if isBreakTime {
-                        viewModel.playSystemSound()
-                        timeRemaining = self.workTime // Reset to work time
-                        isBreakTime = false
-                    } else {
-                        viewModel.playSystemSound()
-                        timeRemaining = self.breakTime // Set to break time
-                        isBreakTime = true
-                    }
-                    isTimerRunning = false
-                }
-            }
-        }
-    }
-    
-    func pauseTimer() {
-        isTimerRunning = false
-        timer?.invalidate()
-        timer = nil
-    }
-    
-    func stopTimer() {
-        isTimerRunning = false
-        timer?.invalidate()
-        timer = nil
-        timeRemaining = self.workTime // Reset to initial work time
-        isBreakTime = false
-    }
-    
-    func nextSession() {
-        if isBreakTime {
-            timeRemaining = self.workTime // Reset to work time
-            isBreakTime = false
-        } else {
-            timeRemaining = self.breakTime // Set to break time
-            isBreakTime = true
-        }
-    }
-    
-    func timeString(time: Int) -> String {
-        let minutes = time / 60
-        let seconds = time % 60
-        return String(format: "%02d:%02d", minutes, seconds)
     }
 }
 
