756676df8e85bbae2ec0810b2c36e5fae3a3e748
Nhat Anh <nmnanh1235@gmail.com>
2024-06-11 19:09:29 +0700

    backup old contentview!

--- Studious/ContentView.swift
+++ Studious/ContentView.swift
@@ -6,27 +6,45 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
-    @ObservedObject var viewModel = ViewModel()
+//    @ObservedObject var viewModel = ViewModel()
     
+    let subjects = ["Math", "Literature", "English"]
+    let timings: [String: (work: Int, break: Int)] = [
+            "Math": (25 * 60, 5 * 60),
+            "Literature": (60 * 60, 10 * 60),
+            "English": (30 * 60, 5 * 60)
+        ]
+    @State private var workTime = 25 * 60
+    @State private var breakTime = 5 * 60
+
+    @State private var timer: Timer?
+    @State private var isTimerRunning = false
+    @AppStorage("isBreakTime") private var isBreakTime = false
+    @AppStorage("timeRemaining") private var timeRemaining = 25 * 60
+    @AppStorage("selectedSubject") private var selectedSubject = "Math" // Default subject
+
     var body: some View {
         ZStack {
-            Color(viewModel.isBreakTime ? .white : .purple).edgesIgnoringSafeArea(.all)
+            Color(isBreakTime ? .white : .purple).edgesIgnoringSafeArea(.all)
             
             VStack {
                 Spacer()
                 
-                Picker(selection: $viewModel.selectedSubject, label: Text(viewModel.selectedSubject)) {
-                    ForEach(viewModel.subjects, id: \.self) { subject in
+                // ComboBox for selecting subjects
+                Picker(selection: $selectedSubject, label: Text(selectedSubject)) {
+                    ForEach(subjects, id: \.self) { subject in
                         Text(subject)
                     }
                 }
-                .pickerStyle(MenuPickerStyle())
-                .foregroundColor(viewModel.isBreakTime ? .blue : .white)
+                .pickerStyle(MenuPickerStyle()) // Style the picker
+                .foregroundColor(isBreakTime ? .blue : .white)
                 .padding()
-                .onChange(of: viewModel.selectedSubject) { newValue in
-                    viewModel.updateTimes(for: newValue)
+                .onChange(of: selectedSubject) { newValue in
+                    updateTimes(for: newValue)
+                    selectedSubject = newValue
                 }
                 
                 ZStack {
@@ -34,23 +52,35 @@ struct ContentView: View {
                         .stroke(lineWidth: 20)
                         .opacity(0.3)
                         .foregroundColor(.gray)
-                        .shadow(color: .black, radius: 10, x: 0, y: 0)
+                        .shadow(color: .black, radius: 10, x: 0, y: 0) // Add shadow effect
                     Circle()
-                        .trim(from: 0, to: CGFloat(viewModel.timeRemaining) / CGFloat(viewModel.isBreakTime ? viewModel.timings[viewModel.selectedSubject]!.break : viewModel.timings[viewModel.selectedSubject]!.work))
+                        .trim(from: 0, to: CGFloat(timeRemaining) / CGFloat(isBreakTime ? self.breakTime : self.workTime)) // Max time for work: 25 minutes, for break: 5 minutes
                         .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .round, lineJoin: .round))
-                        .foregroundColor(viewModel.isBreakTime ? .blue : .green)
+                        .foregroundColor(isBreakTime ? .blue : .green)
                         .rotationEffect(.degrees(-90))
                         .animation(.easeInOut)
                     
-                    Text("\(viewModel.timeString(time: viewModel.timeRemaining))")
+                    Text("\(timeString(time: timeRemaining))")
                         .font(.largeTitle)
-                        .foregroundColor(viewModel.isBreakTime ? .black : .white)
+                        .foregroundColor(isBreakTime ? .black : .white)
                 }
                 .frame(width: 200, height: 200)
                 
                 HStack(spacing: 20) {
                     Button(action: {
-                        viewModel.stopTimer()
+                        if isTimerRunning {
+                            pauseTimer()
+                        } else {
+                            startTimer()
+                        }
+                    }) {
+                        Image(systemName: isTimerRunning ? "pause.circle.fill" : "play.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundColor(isTimerRunning ? .orange : Color(red: 131/255, green: 180/255, blue: 255/255))
+                    }
+                    
+                    Button(action: {
+                        stopTimer()
                     }) {
                         Image(systemName: "stop.circle.fill")
                             .font(.largeTitle)
@@ -58,19 +88,7 @@ struct ContentView: View {
                     }
                     
                     Button(action: {
-                        if viewModel.isTimerRunning {
-                            viewModel.pauseTimer()
-                        } else {
-                            viewModel.startTimer()
-                        }
-                    }) {
-                        Image(systemName: viewModel.isTimerRunning ? "pause.circle.fill" : "play.circle.fill")
-                            .font(.largeTitle)
-                            .foregroundColor(viewModel.isTimerRunning ? .orange : Color(red: 131/255, green: 180/255, blue: 255/255))
-                    }
-                    
-                    Button(action: {
-                        viewModel.nextSession()
+                        nextSession()
                     }) {
                         Image(systemName: "arrow.right.circle.fill")
                             .font(.largeTitle)
@@ -82,6 +100,75 @@ struct ContentView: View {
                 Spacer()
             }
         }
+        .onAppear {
+            updateTimes(for: selectedSubject)
+        }
+    }
+    
+    func startTimer() {
+        isTimerRunning = true
+        if timer == nil {
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                if timeRemaining > 0 {
+                    timeRemaining -= 1
+                } else {
+                    timer?.invalidate()
+                    timer = nil
+                    if isBreakTime {
+                        playSystemSound()
+                        timeRemaining = self.workTime // Reset to work time
+                        isBreakTime = false
+                    } else {
+                        playSystemSound()
+                        timeRemaining = self.breakTime // Set to break time
+                        isBreakTime = true
+                    }
+                    isTimerRunning = false
+                }
+            }
+        }
+    }
+    
+    func pauseTimer() {
+        isTimerRunning = false
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    func stopTimer() {
+        isTimerRunning = false
+        timer?.invalidate()
+        timer = nil
+        timeRemaining = self.workTime // Reset to initial work time
+        isBreakTime = false
+    }
+    
+    func nextSession() {
+        if isBreakTime {
+            timeRemaining = self.workTime // Reset to work time
+            isBreakTime = false
+        } else {
+            timeRemaining = self.breakTime // Set to break time
+            isBreakTime = true
+        }
+    }
+    
+    func updateTimes(for subject: String) {
+        if let times = timings[selectedSubject] {
+            workTime = times.work
+            breakTime = times.break
+        }
+    }
+    
+    func timeString(time: Int) -> String {
+        let minutes = time / 60
+        let seconds = time % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+    
+    func playSystemSound() {
+        let systemSoundID: SystemSoundID = 1304 // You can find different system sound IDs from documentation
+        AudioServicesPlaySystemSound(systemSoundID)
     }
 }
 
